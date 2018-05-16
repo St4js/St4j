ec47fee3c92030c27cf623ca590d4f5858c26317
王木木 <1125334796@qq.com>
2018-05-16 13:56:40 +0800

    常用String操作类

---
+++ src/main/java/com/st4j/net/common/StringUtils.java
@@ -0,0 +1,30 @@
+package com.st4j.net.common;
+
+
+import java.util.Optional;
+
+public class StringUtils {
+	
+
+	public static boolean isEmpty(Object str) {
+	return (str == null || "".equals(str));
+}
+	/**
+	 * 判断对象是否为空，为空返回true，不为空返回false -->JDK1.7-=
+	 * @date	2015-4-3 下午5:51:35
+	 * @param o
+	 * @return
+	 */
+	public static boolean isNull(Object o) {
+		if (null == o || "".equals(o.toString()) || "null".equals(o.toString())) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * 避免空指针,安全的获取map值,为空返回"",不为空则按照实际值返回. -->JDK1.8+=
+	 * */
+	public static String safeGetValue(Object str) {
+		return Optional.ofNullable(str).map(value -> value.toString().trim()).orElse("")+"";
+	}
+}
