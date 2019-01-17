2ec518180489d4dd1f37e0820be0484ba2370587
Administrator <Administrator@192.168.2.108>
2019-01-17 10:42:21 +0800

    公用判断非空

--- src/main/java/com/st4j/net/common/StringUtils.java
+++ src/main/java/com/st4j/net/common/StringUtils.java
@@ -1,6 +1,5 @@
 package com.st4j.net.common;
 
-
 import java.util.Optional;
 
 public class StringUtils {
@@ -11,7 +10,8 @@ public class StringUtils {
 
 	/**
 	 * 判断对象是否为空，为空返回true，不为空返回false -->JDK1.7-=
-	 * @date	2015-4-3 下午5:51:35
+	 * 
+	 * @date 2015-4-3 下午5:51:35
 	 * @param o
 	 * @return
 	 */
@@ -24,8 +24,25 @@ public class StringUtils {
 
 	/**
 	 * 避免空指针,安全的获取map值,为空返回"",不为空则按照实际值返回. -->JDK1.8+=
-	 * */
+	 */
 	public static String safeGetValue(Object str) {
-		return Optional.ofNullable(str).map(value -> value.toString().trim()).orElse("")+"";
+		return Optional.ofNullable(str).map(value -> value.toString().trim()).orElse("") + "";
+	}
+
+	/**
+	 * 判断对象是否为空，为空返回true，不为空返回false -->JDK1.7
+	 * 
+	 * @date 2019-01-17 10:40
+	 * @param booleab
+	 * @return
+	 */
+	public static boolean checkParam(Object... obj) {
+
+		for (Object object : obj) {
+			if ("".equals(object) || object == null) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
